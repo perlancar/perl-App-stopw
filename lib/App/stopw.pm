610caff5e7bf5870f1f1841bff4c75b7f884795b
perlancar (@pc-office) <perlancar@gmail.com>
2014-09-09 17:45:20 +0700

    Initial commit

---
+++ lib/App/stopw.pm
@@ -0,0 +1,18 @@
+package App::stopw;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: A console-based virtual stopwatch and timer
+
+=head1 SYNOPSIS
+
+This distribution provides the C<stopw> command-line utility.
+
+
+=head1 SEE ALSO
+
+L<stopw>
+
+=cut
